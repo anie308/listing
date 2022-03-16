eaf9bb3c524f9f409a6a070b3b494059d2b8bf90
anie308 <aniebietabasiobo@gmail.com>
2022-03-16 08:16:35 +0100

    commit

--- setData.js
+++ setData.js
@@ -7,7 +7,34 @@ document.addEventListener('DOMContentLoaded', ()=>{
     let output = '';
     
     bicycles.forEach((bicycles) => {
-        output+=` `
+        output+=`
+        <a href="./itemPage.html" class="item-link">
+        <div class="item contain">
+            <div class="item-image-con">
+                <img src="./assets/img2.jpg" alt="" class="item-image">
+                <div class="item-tag">$200</div>
+            </div>
+            <div class="item-desc">
+                <div class="item-title">
+                    ${bicycles.title}
+                </div>
+                <div class="item-detail">
+                    Good for outing rides <br/> rides rides
+                </div>
+                <div class="item-action">
+                   <div class="item-price"> </div>
+                   <div class="item-rent">Rent</div>
+                </div>
+
+            </div>
+        </div>
+      </a>
+        
+        
+        
+        
+        `
     })
     
+    listData.innerHTML = output
 });
